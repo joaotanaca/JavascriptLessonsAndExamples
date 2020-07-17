2a0a3d339b61b20fe74c9ea7469aca358bc6bf3a
Tanaca <jtanacar@gmail.com>
2020-07-17 13:25:56 -0300

    Estudando sass

--- 07/11/Matrix.js
+++ 07/11/Matrix.js
@@ -2,29 +2,30 @@
 //aonde o primeiro é o valor que deve se fazer a conta e o segundo se
 //é pra somar ou subtrair com true sendo soma e false sendo subtrair.
 
+const array = [
+  [Math.random() * 10, (Math.random() * 10) > -35],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+  [Math.random() * 10, (Math.random() * 10)> -35 ? false : true],
+];
+
 function calculo(calcArray = []) {
   let calculoTotal = 0;
 
   const callbackForeach = (number) => {
     const verify = number[1];
     const value = number[0];
-    console.log(calculoTotal);
     calculoTotal = verify ? calculoTotal + value : calculoTotal - value;
   };
-
   calcArray.forEach(callbackForeach);
 
-  return calculoTotal;
+  return calculoTotal.toFixed();
 }
 
-const array = [
-  [4, true],
-  [4, false],
-  [4, true],
-  [4, false],
-  [4, true],
-];
-
-const ArrayValues = array.map((array) => array[0]);
-const ArrayCondition = array.map((array) => array[1]);
-console.log(ArrayValues, ArrayCondition);
+console.log(calculo(array));
--- 07/12/SortArray.js
+++ 07/12/SortArray.js
@@ -6,25 +6,33 @@ const sortTwo = (array) => (array[0] > array[1] ? [array[1], array[0]] : array);
 function sortThree(arrayNumbers = []) {
   let aux = 0;
   let filtedArray = arrayNumbers;
+  console.log("Array original: ", arrayNumbers);
+  for (let cont = 0; cont < arrayNumbers.length; cont++) {
+    //Atribuindo o valor da posição i no array ao number : number se torna o valor da posição i do array
+    const number = arrayNumbers[cont];
+    console.log("Array original: ", arrayNumbers);
 
-  for (let i = 0; i < arrayNumbers.length; i++) {
-    const number = arrayNumbers[i];
+    console.log("-----------");
 
-    for (let i2 = i + 1; i2 < arrayNumbers.length; i2++) {
-      const number2 = arrayNumbers[i2];
+    for (let cont2 = cont + 1; cont2 < arrayNumbers.length; cont2++) {
+      console.log("posição:", cont2);
+      const number2 = arrayNumbers[cont2];
+      console.log("value:", number2);
+      console.log("-----------");
 
       if (number > number2) {
         //auxiliar guardando numero maior que o atual
         aux = number2;
 
         //numero da posiçao i2 no array se tornando numero verificado
-        filtedArray[i2] = number;
+        filtedArray[cont2] = number;
 
         //numero na posição i no array se tornado o auxiliar
-        filtedArray[i] = aux;
+        filtedArray[cont] = aux;
       }
     }
   }
   return filtedArray;
 }
-console.log(sortThree([2, 1, 10, 20, 4, 500]));
+
+sortThree([2, 1, 10, 20, 4, 500]);
